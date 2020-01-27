a9453c56f02327d395c8fcd7f224a9807c61f616
Animesh Bhadra <animeshb@archerimagine.com>
2020-01-26 20:02:00 -0600

    -ran-message

--- common/git.sh
+++ common/git.sh
@@ -1,4 +1,7 @@
 # invoke this commnd by just running git-random
 
 git-random(){ gitRan=$(curl -L -s http://whatthecommit.com/ |grep -A 1 "\"c" |tail -1 |sed 's/<p>//'); git commit -m "$gitRan"; }
+git-ran-message(){
+    curl -L -s http://whatthecommit.com/ |grep -A 1 "\"c" |tail -1 |sed 's/<p>//'
+}
 
