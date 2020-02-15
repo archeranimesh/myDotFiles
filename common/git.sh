c84162e1b647b11f8411f0d60aee9c08ccc58bc0
Animesh Bhadra <animeshb@archerimagine.com>
2020-02-15 09:04:37 +0530

    Revert "fuckup".

--- common/git.sh
+++ common/git.sh
@@ -1,2 +1,4 @@
+# invoke this commnd by just running git-random
+
 git-random(){ gitRan=$(curl -L -s http://whatthecommit.com/ |grep -A 1 "\"c" |tail -1 |sed 's/<p>//'); git commit -m "$gitRan"; }
 
